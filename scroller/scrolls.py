36a30b94eb71fafd3d46957380c2defe7a348570
Ellie <epkerns@gmail.com>
2023-03-30 14:04:21 -0600

    stuff

--- scroller/scrolls.py
+++ scroller/scrolls.py
@@ -1,11 +1,9 @@
-import pygame, pytmx
+import pygame
+import random
 
 
 
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
-
-
+# define colors 
 
 BLACK = (0, 0, 0)
 GRAY = (127, 127, 127)
@@ -17,23 +15,48 @@ YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-
-MAP_COLLISION_LAYER = 1
-
-
-background = GRAY
+# Set Screen
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("poop mah goop")
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#load assetts
 
+bg = GRAY
 
+# TODO bg = pygame.image.load("background.jpg").convert()
+player_img = pygame.image.load("randompy/logo.png").convert_alpha()
 
-class Game(object):
-    def __init__(self):
-        self.currentLevelNumber = 0
-        self.levels = []
-        self.levels.append(Level(fileName = #TODO"Assetts/level1.tmx"))
-        self.currentLevel = self.levels[self.currentLevelNumber]
-        
-				self.player = Player(x = 200, y = 100)
-        self.player.currentLevel = self.currentLevel
+#game variables
+player_x = 50
+player_y = 400
+player_speed = 5
+is_jump = False
+jump_count = 10
+level = 1
+
+#Game Loop
+
+running = True
+while running:
+    # Handle Events
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					is_jump = True
+		
+		#Update game state
+		if is_jump:
+			if jump_count >= -10:
+				player_y -= (jump_count * abs(jump_count)) * 0.5
+				jump_count -= 1
+			else:
+				is_jump = False
+				jump_count = 10
+		
+		#Draw the game
+		screen.fill(bg)
+		screen.blit(player_img, (player_x, player_y))
+		pygame.display.flip()
